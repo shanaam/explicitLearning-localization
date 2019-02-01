a0b6613cd9b3d2eb143ece5d604701f9975417a4
Shanaa Modchalingam <s.modcha@gmail.com>
2019-02-01 14:02:37 -0500

    Removed data files and added code to download them from OSF if necessary

--- explicitFunctions.R
+++ explicitFunctions.R
@@ -2,9 +2,70 @@
 # this.dir <- dirname(parent.frame(2)$ofile)
 # setwd(this.dir)
 
-# read in raw data files
 library(ez)
-#library(gplots)
+
+
+# attempt to get data files from OSF project and populate the /data directory
+# THIS IS NOT USED by default. See populateData function
+PopulateData_osf <- function(){
+  library(osfr)
+  library(dplyr)
+  
+  if (length(list.files(path = './data2')) < 3) {
+    osfProject <- osf_retrieve_node('mx5u2')
+    osfDataFiles <- osfProject %>%
+                  osf_ls_files() %>%
+                  filter(name == "data") %>%
+                  osf_ls_files(n_max = 100)
+    
+    # for(i in 1:nrow(osfDataFiles)) {
+    #   row <- osfDataFiles[i,]
+    #   # do stuff with row
+    #   osf_download(row, path = paste("./data2", row$name, sep="/"))
+    # }
+    
+    # #make a list of download links
+    # osfDownloadLinks <- data.frame()
+    # for(i in 1:nrow(osfDataFiles)) {
+    #   row <- osfDataFiles[i,]
+    #   # do stuff with row
+    #   osfDownloadLinks <- c(osfDownloadLinks, row$meta[[1]]$links$download)
+      
+  #save this thing
+  saveRDS(osfDataFiles, file = 'osfDataFiles.rds')
+  }
+}
+
+# load.DownloadDataframe <- function(url,filename) {
+#   
+#   if (file.exists(filename)) {
+#     
+#     df <- read.csv(filename, stringsAsFactors=FALSE)
+#     
+#   } else {
+#     
+#     df <- read.csv(url(url),stringsAsFactors=FALSE)
+#     
+#     write.csv(df,filename,row.names=FALSE,quote=FALSE)
+#     
+#   }
+#   
+#   return(df)
+#   
+# }
+
+# populate the /data directory using the osfDataFiles object
+PopulateData <- function(){
+  osfDataFiles<- readRDS('osfDataFiles.rds') 
+  if (length(list.files(path = './data2')) < 3) {
+    for(i in 1:nrow(osfDataFiles)){
+      row <- osfDataFiles[i,]
+      
+      download.file(url = row$meta[[1]]$links$download, destfile = paste("./data", row$name, sep="/"))
+    }
+  }
+}
+
 
 # Adaptation data
 LoadReachData <- function() {
